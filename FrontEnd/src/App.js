57cd13ae192dc0fb16856e9fbe90b3804bffdff0
Ryan Kelly <s3661678@student.rmit.edu.au>
2020-08-10 17:58:04 +1000

    initial backend

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
+
+function App() {
+  return (
+    <div>
+    <Header/>
+    <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
