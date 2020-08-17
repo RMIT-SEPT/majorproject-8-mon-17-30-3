f4775c613453b59bb8d0566804732ce4e3415ac2
Muhammad <s3779826@student.rmit.edu.au>
2020-08-17 18:29:36 +1000

    Links are working now

--- FrontEnd/React/src/App.js
+++ FrontEnd/React/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
-
+import {BrowserRouter, Route, Link, Switch} from "react-router-dom";
 import './App.css';
-import Form from './Components/Form'
+import Register from './Components/Register'
+import Login from './Components/Login';
+
+
 //import Login from './Components/Login'
 
 function App() {
   return (
 
     <div className="App">
-      <Form />
+      <Login/>
+      <BrowserRouter>
+        <Switch>
+            <Route path="/Login" component={Login}></Route>
+            <Route path="/Register" component={Register}></Route>
+        </Switch>
+      </BrowserRouter>
+
       
     </div>
 
--- FrontEnd/React/src/Components/Register.js
+++ FrontEnd/React/src/Components/Register.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
-import './CSS/style.css'
 import loginImg from '../Components/avatar.png'
+import './CSS/todo.css'
 
-class Login extends Component {
+
+class Register extends Component {
+    
     constructor(props) {
         super(props)
 
         this.state = {
             username: "",
             email: "",
+            firstName: "",
+            lastName: "",
             password: "",
         }
         this.handleSubmit=this.handleSubmit.bind(this)
@@ -23,41 +27,54 @@ class Login extends Component {
             email: event.target.value
         })
     }
+    firsthandler = (event) => {
+        this.setState({
+            firstName: event.target.value
+        })
+    }
+    lasthandler = (event) => {
+        this.setState({
+            lastName: event.target.value
+        })
+    }
     passwordhandler = (event) => {
         this.setState({
             password: event.target.value
         })
     }
-
-
     handleSubmit = (event) => {
-        alert(`${this.state.firstName} ${this.state.lastName}  Login Successfully !`)
+        alert(`${this.state.firstName} ${this.state.lastName}  Registered Successfully !`)
         console.log(this.state);
         this.setState({
             email: "",
             firstName: "",
             lastName: "",
             password: '',
-            gender: "",
+          
         })
      event.preventDefault()
         
     }
 
-    render() {
+render() {
         return (
-            <div className="loginbox">
+            <div className="registerBox">
                 <img src={loginImg} class="avatar"></img>
                 <form onSubmit={this.handleSubmit}>
-               <h1>Login</h1>
+                    <h1>Create Account</h1>
                     <p>UserName </p> <input type="text" value={this.state.username} onChange={this.userNamehandler} placeholder="Username" required /><br />
+                    <p>Email</p> <input type="text" value={this.state.email} onChange={this.emailhandler} placeholder="Email"  required/><br />
+                    <p>FirstName</p> <input type="text" value={this.state.firstName} onChange={this.firsthandler} placeholder="FirstName"  required/><br />
+                    <p>LastName</p> <input type="text" value={this.state.lastName} onChange={this.lasthandler} placeholder="LastName"  required/><br />
                     <p>Password</p> <input type="password" value={this.state.password} onChange={this.passwordhandler} placeholder="Password" required /><br />
-                    <br/><input type="submit" value="Sign in" /> 
-                    <a href="/Register">Create Account</a>
-            </form>        
-            </div>
+                    <br/><input type="submit" value="Register Account" />
+                    <a href="Login">Back to Login</a>
+                </form>
 
+            </div>
+            
         )
     }
 }
-export default Login
+
+export default Register
