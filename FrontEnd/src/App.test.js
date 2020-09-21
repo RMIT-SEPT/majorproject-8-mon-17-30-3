b2f42158b59f4f0ed530e389f18ab55c90d792fd
Ryan Kelly <s3661678@student.rmit.edu.au>
2020-09-21 16:56:43 +1000

    updated app.test.js

--- FrontEnd/src/App.test.js
+++ FrontEnd/src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import {shallow, mount} from "enzyme";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+Enzyme.configure({ adapter: new Adapter()});
+
+describe(" Make Booking tests", ()=>{
+    const mockFn = jest.fn();
+})
+it("should render app", ()=> {
+    const component = shallow(<App />);
+    expect(component).toHaveLength(1);
+})
