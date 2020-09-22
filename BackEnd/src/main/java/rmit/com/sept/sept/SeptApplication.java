2eab9e096db44fb13d491f0fc74b00e382bcc349
s3743761 <49186141+s3743761@users.noreply.github.com>
2020-09-22 21:56:32 +1000

    Update SeptApplication.java

--- BackEnd/src/main/java/rmit/com/sept/sept/SeptApplication.java
+++ BackEnd/src/main/java/rmit/com/sept/sept/SeptApplication.java
@@ -17,19 +17,15 @@ public class SeptApplication {
 	public static void main(String[] args) {
 		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(); 
 		String encodedPassword = passwordEncoder.encode("password");
-		System.out.println(password);
-//		System.out.print(encodedPassword);
+	
 		try { 
 			String url = "jdbc:mysql://127.0.0.1:3306/sept?useLegacyDatetimeCode=false&serverTimezone=UTC&createDatabaseIfNotExist=true"; 
 	        Connection conn = DriverManager.getConnection(url,"root",password);
 	        Statement st = conn.createStatement(); 
 	        st.executeUpdate("CREATE TABLE role(role_id INT(50) NOT NULL AUTO_INCREMENT,role_desc VARCHAR(50), role_name VARCHAR(50), CONSTRAINT role_pk PRIMARY KEY(role_id))");
-//	        st.executeUpdate("INSERT IGNORE INTO user(user_id,email,lastname,firstname, password, status)VALUES(6,'admin2@admin2.com','admin2','admin2','"+ encodedPassword +"','VERIFIED')");
+
 	        st.executeUpdate("INSERT IGNORE INTO role(role_id,role_desc,role_name)VALUES(1,'ADMIN_USER','ADMIN_USER')");
 	        st.executeUpdate("INSERT IGNORE INTO role(role_id,role_desc,role_name)VALUES(2,'SITE_USER','SITE_USER')");
-//	        st.executeUpdate("INSERT IGNORE INTO auth_user_role(auth_user_id,auth_role_id) VALUES (6,1)");
-	    
-//	        st.executeQuery("select u.email, r.role_name from user u inner join auth_user_role ur on(u.user_id=ur.auth_user_id) inner join role r on(ur.auth_role_id=r.role_id)");
 
 	        conn.close();
 		} catch (Exception e) { 
